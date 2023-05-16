6bf415a0c637b2e96c91ddd9f2837b1e5776359e
Werw <allvxm4@gmail.com>
2023-05-16 15:15:59 +0300

    ts store

--- src/utils/hooks/useForm.ts
+++ src/utils/hooks/useForm.ts
@@ -9,12 +9,14 @@ type UseFormReturnType = {
     handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
     setValues: (values: FormValues) => void;
 };
+
 export function useForm(inputValues: FormValues): UseFormReturnType {
     const [values, setValues] = useState<FormValues>(inputValues);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const {value, name} = e.target;
-        setValues({...values, [name]: value});
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        const { value, name } = e.target;
+        setValues({ ...values, [name]: value });
     };
-    return {values, handleChange, setValues};
+
+    return { values, handleChange, setValues };
 }
